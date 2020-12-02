6c631bfea9c9264d13450b0d0044f60a98c655eb
evio <evio@vip.qq.com>
2020-12-02 15:03:39 +0800

    update

--- packages/typeorm/src/index.ts
+++ packages/typeorm/src/index.ts
@@ -1,10 +1,11 @@
 import { Server } from '@dubbo.ts/server';
-import { createConnection, ConnectionOptions } from 'typeorm';
+import { createConnection, ConnectionOptions, Connection } from 'typeorm';
 export const typeorm = Symbol('typeorm');
 export function useTypeOrm(server: Server, options: ConnectionOptions) {
+  let connection: Connection;
+  server.container.bind(typeorm).toDynamicValue(() => connection);
   server.application.on('mounted', async () => {
-    const connection = await createConnection(options);
-    server.container.bind(typeorm).toConstantValue(connection);
+    connection = await createConnection(options);
     server.application.on('unmounted', () => connection.close());
   });
 }
